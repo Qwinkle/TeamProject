ebfbcef5c774c149601a21b1b94e7b936b0e4a4c
James Britton <Jbritton@stu.jsu.edu>
2018-10-09 14:07:25 -0500

    Added Badge and starting of Push

---
+++ src/teamproject/Badge.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TeamProject;
+
+/**
+ *
+ * @author JayBritt
+ */
+public class Badge {
+    private String id;
+    private String Desc;
+
+    public Badge(String id, String Desc) {
+        this.id = id;
+        this.Desc = Desc;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDesc() {
+        return Desc;
+    }
+
+    public void setDesc(String Desc) {
+        this.Desc = Desc;
+    }
+
+    
+    
+    
+    
+}
