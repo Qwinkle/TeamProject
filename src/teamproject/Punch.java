6ed51670ade51e46c95725c9905de1c7986de3ee
James Britton <Jbritton@stu.jsu.edu>
2018-10-11 13:48:58 -0500

    Finished Punch class

--- src/teamproject/Punch.java
+++ src/teamproject/Punch.java
@@ -1,9 +1,66 @@
 package teamproject;
 
-import java.util.Calendar;
 
+import java.util.GregorianCalendar;
 public class Punch {
     private int terminalId, punchTypeId;
     private Badge badge;
-    private long timeStamp;
+    private long timestamp;
+    private GregorianCalendar cal = new GregorianCalendar();
+    
+
+    public Punch(int terminalId, int punchTypeId, Badge badge, long timestamp) {
+        this.terminalId = terminalId;
+        this.punchTypeId = punchTypeId;
+        this.badge = badge;
+        this.timestamp = timestamp;
+        cal.setTimeInMillis(timestamp);
+        
+    }
+    public String printOriginalTimeStamp(){
+        return null;
+    }
+
+    public int getTerminalId() {
+        return terminalId;
+    }
+
+    public void setTerminalId(int terminalId) {
+        this.terminalId = terminalId;
+    }
+
+    public int getPunchTypeId() {
+        return punchTypeId;
+    }
+
+    public void setPunchTypeId(int punchTypeId) {
+        this.punchTypeId = punchTypeId;
+    }
+
+    public Badge getBadge() {
+        return badge;
+    }
+
+    public void setBadge(Badge badge) {
+        this.badge = badge;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public GregorianCalendar getCal() {
+        return cal;
+    }
+
+    public void setCal(GregorianCalendar cal) {
+        this.cal = cal;
+    }
+    
+  
+  
 }
