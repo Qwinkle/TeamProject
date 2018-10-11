f6267bfe1219f0b7b0bbeb51199fa656a011679c
Christian Price <cprice2@stu.jsu.edu>
2018-10-11 14:08:50 -0500

    Revised Badge

---
+++ src/teamproject/TASDatabase.java
@@ -0,0 +1,31 @@
+package teamproject;
+
+/**
+ *
+ * @author qwink
+ */
+public class TASDatabase {
+
+    public TASDatabase() {
+        
+    }
+    
+    private Badge badge;
+    private Punch push;
+    private Shift shift;
+    
+    public Badge getBadge() {
+        return badge;
+    }
+
+    public Punch getPush() {
+        return push;
+    }
+
+    public Shift getShift() {
+        return shift;
+    }
+    
+    
+    
+}
